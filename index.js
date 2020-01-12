fc49b2325a9a69900074c1e00933331db7809eff
“Name <pchelenkovartem@gmail.com>
2020-01-12 13:38:58 +0100

    ready HEX creator

---
+++ index.js
@@ -0,0 +1,16 @@
+const hexColorNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D' ,'E' ,'F'];
+const hexBtn = document.querySelector('.colorBtn');
+const bodyBtn = document.querySelector('body');
+const hexName = document.querySelector('.hexName');
+
+hexBtn.addEventListener('click', getHex);
+
+function getHex() {
+    let hexColor = "#";
+    for(let i=0; i<6; i++) {
+        let random = Math.floor(Math.random()*hexColorNum.length);
+        hexColor +=hexColorNum[random];
+        bodyBtn.style.backgroundColor = hexColor;
+        hexName.innerHTML = hexColor;
+    }
+}
