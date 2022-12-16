28ed1de96263f5d2d1f56b72bc2ee5dcf96700e4
ArtemPchela <pchelenkovartem@gmail.com>
2022-12-16 17:17:01 +0100

    all updates in README file

---
+++ README.md
@@ -0,0 +1,13 @@
+# JavaScript Color Generator
+
+### Functionality:
+* Generator Hex colors
+* Copy color with clipboard
+
+-[X] create generator hex colors
+-[X] create copy generated color button (clipboard)
+-[X] popup window with success color copy 
+-[X] close popup window function
+-[ ] popup window should be on the top of content
+-[ ] popup window show and hide with smoothly
+-[ ] all content should generate in the index.js
--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-const hexColorNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D' ,'E' ,'F'];
+/**
+ * for colrs generation
+ * */
+
+const hexColorNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const hexBtn = document.querySelector('.colorBtn');
 const bodyBtn = document.querySelector('body');
 const hexName = document.querySelector('.hexName');
 
+/**
+ * @copy actions
+ * **/
+const btnCopy = document.getElementById('btnCopy')
+const copyText = document.getElementById('copyText')
+
+/**
+ * @type modal
+ */
+const modal = document.querySelector('#modal');
+const typeOfColor = document.getElementById('typeOfColor');
+
+
 hexBtn.addEventListener('click', getHex);
 
 function getHex() {
-    let hexColor = "#";
-    /*you may change in cycle for i<3*/
-    for(let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random()*hexColorNum.length);
-        hexColor +=hexColorNum[random];
-        bodyBtn.style.backgroundColor = hexColor;
-        hexName.innerHTML = hexColor;
-    }
+  let hexColor = "#";
+  /*you may change in cycle for i<3*/
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexColorNum.length);
+    hexColor += hexColorNum[random];
+    bodyBtn.style.backgroundColor = hexColor;
+    hexName.innerHTML = hexColor;
+    typeOfColor.innerText = hexColor
+  }
 }
+
+getHex()
+
+btnCopy.addEventListener('click', function copyDivToClipboard() {
+  var r = document.createRange();
+  r.selectNode(document.getElementById("selected"));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  modal.style.display = 'block';
+})
+
+
+modal.addEventListener('click', function () {
+  document.getElementById('modal').style.display = 'none';
+});
